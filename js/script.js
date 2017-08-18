3650899c32a9a07f2dd90f7c3c8e7946ada294c6
ANNA Skowronek-Jaskuła <annaskowronek-jaskula@MacBook-Air-ANNA.local>
2017-08-19 00:08:20 +0200

    corrected interval timing

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
-var carouselList = $("#carousel ul");
+
 $(function(){
 
+var carouselList = $("#carousel");
 
 	setInterval(function(changeSlide) {
-		carouselList.animate({'marginLeft':-400}, 500, (function(moveFirstSlide) {
+		carouselList.animate({ marginLeft: '-400px' }, 500, 
+			(function(moveFirstSlide) {
 
-			var firstItem = carouselList.find("li:first");
-			var lastItem = carouselList.find("li:last");
-			lastItem.after(firstItem);
-			carouselList.css({marginLeft:0});
-
-		}), 1000);
-	});	
+				var firstItem = carouselList.find("li:first");
+				var lastItem = carouselList.find("li:last");
+				lastItem.after(firstItem);
+				carouselList.css({marginLeft: '0'});
+			}),
+		);
+	}, 1000);
 });
