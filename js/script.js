7d642cc3662a58a51b4711b2b48878054a0986ab
ANNA Skowronek-Jaskuła <annaskowronek-jaskula@MacBook-Air-ANNA.local>
2017-08-28 15:10:41 +0200

    Rev 2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,42 @@
 
 var carouselList = $("#carousel ul");
 
+var firstItem = carouselList.find("li:first");
+var lastItem = carouselList.find("li:last");
+
+setInterval(changeSlide, 3000);
+
+
+
+function changeSlide() {
+	carouselList.find("li:first").clone().appendTo(carouselList);
+	carouselList.animate({
+		marginLeft: '-250px', width: '1750px', marginRight: '-250px'
+		},
+		1000,
+		moveFirstSlide
+	);
+}
+function moveFirstSlide() {
+
+	carouselList.find("li:first").remove();
+	carouselList.animate({
+		marginLeft: '0', width: '1250px', marginRight: '0'
+		},
+		0
+	);
+}
+
+
+ 
+
+
+
+
+
+/*
+var carouselList = $("#carousel ul");
+
 setInterval(changeSlide, 3000);
 
 function changeSlide() {
@@ -10,8 +46,6 @@ function changeSlide() {
 		moveFirstSlide
 	);
 }
-
-
 function moveFirstSlide() {
 	var firstItem = carouselList.find("li:first");
 	var lastItem = carouselList.find("li:last");
@@ -22,4 +56,12 @@ function moveFirstSlide() {
 		{marginLeft: '0'},
 		0
 	);
+	cloneFirstSlide()
 }
+
+
+function cloneFirstSlide() {
+$( "#carousel ul li" ).first().clone().prependTo( "ul" );
+}
+*/
+
